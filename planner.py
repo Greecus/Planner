aa7a052f0e1f021e684ec9bfe92a923e76278d7c
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2023-10-30 15:48:32 +0100

    Finished TreeCell class structure added method for returning all visible branches

--- planner.py
+++ planner.py
@@ -1,14 +1,17 @@
 import os
+from typing import NewType
 
 clear = lambda: os.system("cls")
 
 class TreeCell:
-    __slots__ = ("_name","_sub_cells","_open","_note")
-    def __init__(self,name:str,sub_cells:list=None,note:str=''):
+    __slots__ = ("_name","_sub_cells","_open","_note",'_depth')
+    def __init__(self,name:str,sub_cells:list=None,note:str='',open:bool = True):
         self._name = name
-        self._sub_cells=sub_cells
-        self._open=False
+        if sub_cells:self._sub_cells=sub_cells
+        else: self._sub_cells=[]
+        self._open=open
         self._note=note
+        self._depth=0
 
     @property
     def sub_cells(self):
@@ -22,9 +25,27 @@ class TreeCell:
             if not isinstance(cell,TreeCell): raise TypeError('sub_cells should be a list of TreeCell instances or empty list')
         self._sub_cells=value
     
-    def __add__(self,new_cell):
+    @property
+    def open(self):
+        return self._open
+    
+    @open.setter
+    def open(self,value):
+        if not isinstance(value,bool): raise TypeError('open should be True or False')
+        value=bool(value)
+        self._open=value
+
+    '''def __getitem__(self,key):
+        return self._sub_cells[key]'''
+    
+    '''def __setitem__(self,key,value):
+        self._sub_cells[key]+=value'''
+
+    def __iadd__(self,new_cell):
         if not isinstance(new_cell,TreeCell): raise TypeError("Only instances of TreeCell class can be added")
+        new_cell._depth=self._depth+1
         self._sub_cells.append(new_cell)
+        return self
 
     def __bool__(self):
         if self._sub_cells: return True
@@ -36,10 +57,22 @@ class TreeCell:
     def __iter__(self):
         return iter(self._sub_cells)
     
+    def show_branches(self,prefix:str='  ')->list:
+        branch_list=[self]
+        if self._open and bool(self):
+            for cell in self._sub_cells:
+                branch_list.extend(cell.show_branches(prefix))
+        return branch_list
+    
+
+    
 
 
 if __name__=='__main__':
-    a=TreeCell('name')
-    a.sub_cells=[TreeCell('name')]
-    print(bool(a))
-    print(list(a))
+    a=TreeCell('1')
+    print(False or bool(a))
+    a+=TreeCell('2')
+    a.sub_cells[0]+=TreeCell('3')
+    branches=a.show_branches()
+    for cell in branches:
+        print(cell._depth*' '+str(cell))
